b96638e57b9a24a1d5746971d9dedcd5752cb76d
Apeksha Dhungana <dipikadhungana86@gmail.com>
2025-09-19 14:12:05 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Skills from './components/Skills';
-import Experience from './components/Experience';
-import Work from './components/Work';
-import Education from './components/Education';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import BlurBlob from './components/BlurBlob';
-import gift from './assets/gift.gif';
+import Navbar from './components/Navbar/Navbar.jsx';
+import About from './components/About/About.jsx';
+import Skills from './components/Skills/Skills.jsx';
+import Experience from './components/Experience/Experience.jsx';
+import Work from './components/Work/Work.jsx';
+import Education from './components/Education/Education.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import BlurBlob from './components/BlurBlob.jsx';
+// import gift from './assets/gift.gif';
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
 
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       
-      <img src={gift} alt="Gift Animation" />
+      {/* <img src={gift} alt="Gift Animation" /> */}
 
       <div className="relative pt-20">
         <Navbar />
