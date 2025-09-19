daba9a2c0a50b387d1f6b3aac604716aaf94bdef
Apeksha Dhungana <dipikadhungana86@gmail.com>
2025-09-19 13:47:04 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import About from "./components/About/About";
-import Skills from "./components/Skills/Skills";
-import Experience from "./components/Experience/Experience";
-import Work from "./components/Work/Work";
-import Education from "./components/Education/Education";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
+import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Work from './components/Work';
+import Education from './components/Education';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import BlurBlob from './components/BlurBlob';
+import gift from './assets/gift.gif';
 
 const App = () => {
   return (
     <div className="bg-[#050414]">
-
-       <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
+      <BlurBlob position={{ top: '35%', left: '20%' }} size={{ width: '30%', height: '40%' }} />
 
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      
+      <img src={gift} alt="Gift Animation" />
 
       <div className="relative pt-20">
         <Navbar />
@@ -27,7 +29,6 @@ const App = () => {
         <Contact />
         <Footer />
       </div>
-
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import React from 'react'; 
-import GiftCard from './components/GiftCard';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-      <GiftCard />
-      <App />
-    </>
+    <App />
   </React.StrictMode>
 );
