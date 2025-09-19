42c011bc4345c1b6b7fc5d38493160d290bebc90
Apeksha Dhungana <dipikadhungana86@gmail.com>
2025-09-19 13:58:39 +0545

    add

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// GitHub Pages host को लागि
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  base: '/Portfolio/', // GitHub Pages मा project को path
   plugins: [react()],
-});
+})
