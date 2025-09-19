ace6dc1180903fdb581c7305927d71bc25e1c62e
Apeksha Dhungana <dipikadhungana86@gmail.com>
2025-09-19 12:47:46 +0545

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio',  // replace <portfolio> with your repo name if using GitHub Pages without custom domain
+  base: '/Portfolio',  // replace <portfolio> with your repo name if using GitHub Pages without custom domain
   plugins: [react()],
 })
